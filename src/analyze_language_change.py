330728de4c7b57a9f8c8641b855698dd829c9d5c
julilili42 <116183344+julilili42@users.noreply.github.com>
2025-10-24 19:11:47 +0200

    added notebook

--- src/analyze_language_change.py
+++ src/analyze_language_change.py
@@ -1,16 +1,10 @@
 from datasets import load_dataset
 from pydantic import BaseModel, TypeAdapter
 import pandas as pd
-from itertools import islice, chain
-from sklearn.feature_extraction.text import CountVectorizer
+from itertools import islice
 from datetime import date
 from typing import Iterator
-import multiprocessing as mp
 import numpy as np 
-from collections import defaultdict
-import seaborn as sns
-import matplotlib.pyplot as plt
-
 
 class Article(BaseModel):
   date: date
@@ -22,6 +16,7 @@ class Article(BaseModel):
 
 def import_corpus(batch_size: int, url: str, num_batches: int | None = None, split: str ="train", streaming: bool =True) -> Iterator[Article]:
   ds = load_dataset(url, split=split, streaming=streaming)
+
   adapter = TypeAdapter(list[Article])
 
   batch_counter = 0
@@ -58,69 +53,3 @@ def linear_prediction(col: pd.Series, year_of_prediction: int=2024) -> float:
     pred = m * year_of_prediction + b
     
     return float(np.clip(pred, 0.0, 1.0))
-
-
-
-def main():
-  corpus_18_23 = import_corpus(batch_size=100, streaming=False, url="bjoernp/tagesschau-2018-2023")
-  corpus_24 = import_corpus(batch_size=100, streaming=False, url="bjoernp/tagesschau-010124-020524")
-
-  corpus = chain(corpus_18_23, corpus_24)
-
-  rows = [(a.date.year, a.article) for a in corpus]
-  year, articles = zip(*rows)
-
-  # generate word matrix 
-  vec = CountVectorizer(max_features=100000)
-  word_matrix = vec.fit_transform(articles)
-  vocab = vec.get_feature_names_out()
-
-  word_matrix_bin = word_matrix.sign()
-
-  unique_years = np.unique(year)
-  
-  # distribution of articles in unique years
-  year_distribution = defaultdict(int)
-
-  # dimension unique_years x vocab
-  rows = []
-
-  for y in unique_years:
-      sel = (year == y)
-      # number of articles in year y
-      n = int(sel.sum())
-      year_distribution[int(y)] = n
-      
-      # count how many articles (of this year) contain each word at least once -> dimension 1 x n 
-      present_counts = np.asarray(word_matrix_bin[sel].sum(axis=0)).ravel()
-      rows.append(present_counts / max(n, 1))
-  
-  # occurence_freq per year
-  occurence_freq = pd.DataFrame(rows, index=unique_years, columns=vocab)
-
-  
-  # goal: try to find words which disproportionaly increased in use 
-  # dataframe containing frequencies before introduction of llm's
-  train = occurence_freq.loc[2018:2022]
-  
-  # linear interpolation, frequency value of 2024
-  q = train.apply(linear_prediction, axis=0)
-
-  # empirical frequency
-  p = occurence_freq.loc[2024]
-
-  # Metric 1: frequency gap delta = p - q
-  delta = p - q
-
-  # Metric 2: frequency ratio r = p/q
-  r = p / q.replace(0, 1e-8)
-  sorted_freq_gap = delta.sort_values(ascending=False).head(100)
-  sorted_freq_ratio = r.sort_values(ascending=False).head(100)
-
-  print(sorted_freq_gap)
-  print(sorted_freq_ratio)
-
-  
-if __name__ == "__main__":
-  mp.set_start_method("fork", force=True)
-  main()
