d45aab5343e13298b547f61ebd18a42c9c643dd3
julilili42 <116183344+julilili42@users.noreply.github.com>
2025-10-25 14:48:47 +0200

    add excess words

--- src/analyze_language_change.py
+++ src/analyze_language_change.py
@@ -5,6 +5,9 @@ from itertools import islice
 from datetime import date
 from typing import Iterator
 import numpy as np 
+from typing import Sequence
+from scipy import sparse
+
 
 class Article(BaseModel):
   date: date
@@ -41,15 +44,35 @@ def import_corpus(batch_size: int, url: str, num_batches: int | None = None, spl
     # validating the structure of the json batch enables easy access of the fields
     yield from adapter.validate_python(batch) 
 
+"""
+produces years x vocab occurrence frequencies matrix
+"""
+def occurrence_freq_by_year(
+    year: np.ndarray,                  # shape: year
+    word_matrix_bin: sparse.spmatrix,  # shape: year x vocab, binary (>=1 -> 1)
+    vocab: Sequence[str]
+) -> pd.DataFrame:
+    unique_years = np.sort(np.unique(year))
+    # dimension unique_years x vocab
+    rows = []
+    for y in unique_years:
+        sel = (year == y)
+        # number of articles in year y
+        n = int(sel.sum())
+        # count how many articles (of this year) contain each word at least once -> dimension 1 x n 
+        present_counts = np.asarray(word_matrix_bin[sel].sum(axis=0)).ravel()  
+        rows.append(present_counts / max(n, 1))
+    return pd.DataFrame(rows, index=unique_years, columns=vocab)
 
 
-def linear_prediction(col: pd.Series, year_of_prediction: int=2024) -> float:
+def linear_prediction(col: pd.Series, target_year: int=2024) -> float:
     y = col.dropna()
     x = y.index.values.astype(float)
     
     # linear fit 
     m, b = np.polyfit(x, y.values, deg=1)
 
-    pred = m * year_of_prediction + b
+    pred = m * target_year + b
     
+    # clamp to [0,1]
     return float(np.clip(pred, 0.0, 1.0))
